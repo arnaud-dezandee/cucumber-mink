0d59c21c2c290f335ab6e47cdb7042ed0c3cd07b
Arnaud Dezandee <dezandee.arnaud@gmail.com>
2015-04-23 17:36:18 +0200

    Licensing lib files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp   = require('gulp'),
+    header = require('gulp-header'),
     eslint = require('gulp-eslint');
 
 gulp.task('lint', function() {
@@ -6,6 +7,24 @@ gulp.task('lint', function() {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
+});
+
+gulp.task('licensing', function() {
+  var license = [
+    '/**',
+    ' * The MIT License (MIT)',
+    ' *',
+    ' * Copyright (c) 2015 Arnaud Dezandee',
+    ' *',
+    ' * Authors:',
+    ' *     Arnaud Dezandee <dezandee.arnaud@gmail.com>',
+    ' */',
+    '\n'
+  ].join('\n');
+
+  return gulp.src('./lib/**/*.js')
+    .pipe(header(license))
+    .pipe(gulp.dest('./lib/'));
 });
 
 gulp.task('default',  ['lint']);
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Arnaud Dezandee
+ *
+ * Authors:
+ *     Arnaud Dezandee <dezandee.arnaud@gmail.com>
+ */
+
 module.exports = require('./lib/mink');
